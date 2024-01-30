1a0b8e8e340db888a93042df34a47542fe1fa89a
Matthew Pomes <matthew.pomes@pm.me>
2024-01-30 11:14:55 -0600

    Add name support

--- nom-tracable-macros/src/lib.rs
+++ nom-tracable-macros/src/lib.rs
@@ -4,7 +4,7 @@ extern crate proc_macro;
 
 use crate::proc_macro::TokenStream;
 use quote::ToTokens;
-use syn::{self, parse_macro_input, parse_quote, AttributeArgs, FnArg, ItemFn, Stmt};
+use syn::{self, parse_macro_input, parse_quote, AttributeArgs, FnArg, ItemFn, NestedMeta, Stmt};
 
 #[proc_macro_attribute]
 pub fn tracable_parser(attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -13,9 +13,15 @@ pub fn tracable_parser(attr: TokenStream, item: TokenStream) -> TokenStream {
     impl_tracable_parser(&attr, &item)
 }
 
-fn impl_tracable_parser(_attr: &AttributeArgs, item: &ItemFn) -> TokenStream {
+fn impl_tracable_parser(attr: &AttributeArgs, item: &ItemFn) -> TokenStream {
+    let ident = &item.sig.ident;
+    let name = match attr.first() {
+        Some(NestedMeta::Lit(name)) => name.to_token_stream(),
+        _ => parse_quote!(stringify!(#ident)),
+    };
+
     let default = impl_tracable_parser_default(&item);
-    let trace = impl_tracable_parser_trace(&item);
+    let trace = impl_tracable_parser_trace(&item, name);
 
     let mut item = item.clone();
 
@@ -36,9 +42,7 @@ fn impl_tracable_parser_default(item: &ItemFn) -> Stmt {
     }
 }
 
-fn impl_tracable_parser_trace(item: &ItemFn) -> Stmt {
-    let ident = &item.sig.ident;
-
+fn impl_tracable_parser_trace(item: &ItemFn, name: impl ToTokens) -> Stmt {
     let input = if let Some(x) = &item.sig.inputs.first() {
         match x {
             FnArg::Typed(arg) => &arg.pat,
@@ -53,14 +57,14 @@ fn impl_tracable_parser_trace(item: &ItemFn) -> Stmt {
     parse_quote! {
         #[cfg(feature = "trace")]
         {
-            let (depth, #input) = nom_tracable::forward_trace(#input, stringify!(#ident));
+            let (depth, #input) = nom_tracable::forward_trace(#input, #name);
 
             let body_ret = {
                 let body = || { #body };
                 body()
             };
 
-            nom_tracable::backward_trace(body_ret, stringify!(#ident), depth)
+            nom_tracable::backward_trace(body_ret, #name, depth)
         }
     }
 }
